bf5376b05d74cb3d26d083833860cc0fc06cc4ba
Rumeysa Başaran <126478668+rumeysabasaran@users.noreply.github.com>
2025-05-31 19:45:38 +0300

    Add files via upload

---
+++ MatrisÇarpım/Matrisİşlem_SwitchCase/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrisİşlem_SwitchCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Matrisin ilk boyutu kaç olsun?: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Matrisin ikinci boyutu kaç olsun: ");
+            int b = int.Parse(Console.ReadLine());
+
+            int[,] M = new int[a, b];
+            int[,] N = new int[a, b];
+
+            Random rnd = new Random();
+
+            Console.WriteLine("---------- M MATRİSİ ----------");
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = 0; j < b; j++)
+                {
+                    M[i, j] = rnd.Next(10, 100);
+                    Console.Write(M[i, j] + "   ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("---------- N MATRİSİ ----------");
+            for (int i = 0; i < a; ++i)
+            {
+                for (int j = 0; j < b; j++)
+                {
+                    N[i, j] = rnd.Next(10, 100);
+                    Console.Write(N[i, j] + "   ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Yapmak istediğiniz işlemi seçin:");
+            Console.WriteLine("1 - Matris Toplama");
+            Console.WriteLine("2 - Matris Çıkarma");
+            Console.WriteLine("3 - Skaler Çarpım (Sabit sayı ile çarpma)");
+            Console.Write("Seçiminiz: ");
+            int secim = int.Parse(Console.ReadLine());
+
+            int[,] P = new int[a, b];
+
+            switch (secim)
+            {
+                case 1:
+                    Console.WriteLine("----- TOPLAMA MATRİSİ -----");
+                    for (int i = 0; i < a; i++)
+                    {
+                        for (int j = 0; j < b; j++)
+                        {
+                            P[i, j] = M[i, j] + N[i, j];
+                            Console.Write(P[i, j] + "   ");
+                        }
+                        Console.WriteLine();
+                    }
+                    break;
+
+                case 2:
+                    Console.WriteLine("----- ÇIKARMA MATRİSİ -----");
+                    for (int i = 0; i < a; i++)
+                    {
+                        for (int j = 0; j < b; j++)
+                        {
+                            P[i, j] = M[i, j] - N[i, j];
+                            Console.Write(P[i, j] + "   ");
+                        }
+                        Console.WriteLine();
+                    }
+                    break;
+
+                case 3:
+                    Console.Write("Skaler çarpım için bir sayı girin: ");
+                    int skaler = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("----- SKALER ÇARPIM MATRİSİ (M * " + skaler + ") -----");
+                    for (int i = 0; i < a; i++)
+                    {
+                        for (int j = 0; j < b; j++)
+                        {
+                            P[i, j] = M[i, j] * skaler;
+                            Console.Write(P[i, j] + "   ");
+                        }
+                        Console.WriteLine();
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("Geçersiz seçim!");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MatrisÇarpım/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrisÇarpım
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Matrisin ilk boyutunu giriniz: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Matrisin ikinci boyutunu giriniz: ");
+            int b = int.Parse(Console.ReadLine());
+            Random rnd = new Random();
+
+            Console.WriteLine();
+            int[,] A = new int[a, b];
+            Console.WriteLine("---------- A MATRİSİ ----------");
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = 0; j < b; j++)
+                {
+                    A[i, j] = rnd.Next(1,6);
+                    Console.Write(A[i, j] + "  ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.Write("Matrisin ilk boyutunu giriniz: ");
+            int c = int.Parse(Console.ReadLine());
+            Console.Write("Matrisin ikinci boyutunu giriniz: ");
+            int d = int.Parse(Console.ReadLine());
+            int[,] B = new int[c, d];
+
+            Console.WriteLine("---------- B MATRİSİ ----------");
+            for (int i = 0; i < c; i++)
+            {
+                for (int j = 0; j < d; j++)
+                {
+                    B[i, j] = rnd.Next(1, 6);
+                    Console.Write(B[i, j] + "  ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("--------- ÇARPIM SONUCU ----------");
+
+            if ((A.GetLength(1)) == (B.GetLength(0)))
+            {
+                int[,] C = new int[a, d];
+                for (int i = 0; i < (A.GetLength(0)); i++)
+                {
+                    for (int j = 0; j < (B.GetLength(1)); j++)
+                    {
+                        C[i, j] = 0;
+                        for (int k = 0; k < (A.GetLength(1)); k++)
+                        {
+                            C[i, j] += A[i, k] * B[k, j];
+                        }
+                        Console.Write(C[i, j] + "  ");
+
+                    }
+                    Console.WriteLine();
+                }
+         
+            }
+            Console.Read();
+        }
+    }
+}
