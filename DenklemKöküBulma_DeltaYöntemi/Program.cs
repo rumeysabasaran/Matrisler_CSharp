c882bb7d6982ea66cc1e3fd5ff1fd06219b9644e
Rumeysa Başaran <126478668+rumeysabasaran@users.noreply.github.com>
2025-06-01 19:50:11 +0300

    Add files via upload

---
+++ DenklemKöküBulma_DeltaYöntemi/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DenklemKöküBulma_DeltaYöntemi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ekrandan girilen ax^2 + bx+c = 0 denkleminin köklerini Delta Yöntemi ile bulalım.
+            int a = 0, b = 0, c = 0;
+            Console.Write("Denklem giriniz: ");
+            string ax = Console.ReadLine();
+
+            string[] s = ax.Split('x');
+            string[] t = s[1].Split(' ');
+            string[] v = s[2].Split(' ');
+
+            a = Convert.ToInt32(s[0]);
+            b = Convert.ToInt32(t[2]);
+            c = Convert.ToInt32(v[2]);
+
+            Console.WriteLine("Denklem: {0}x^2 + {1}x + {2} = 0", a, b, c);
+            Console.WriteLine("a= {0}, b = {1}, c = {2}", a, b, c);
+
+            int delta = b*b - 4 * a * c;
+
+            Console.WriteLine("Delta: {0}", delta);
+
+            if (delta > 0)
+            {
+                double kok1 = (-b - Math.Sqrt(delta)) / (2 * a);
+                double kok2 = (-b + Math.Sqrt(delta)) / (2 * a);
+                Console.WriteLine("Denklemin iki farklı kökü vardır.");
+                Console.WriteLine("Kök 1: {0}, Kök2: {1}", kok1, kok2);
+
+            }
+            else if (delta == 0)
+            {
+                double kok1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                Console.WriteLine("İki kök birbirine eşit,Kök1={0}", kok1);
+            }
+            else
+            {
+                Console.WriteLine("Denklemin reel kökü yoktur.");
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DenklemKöküBulma_DeltaYöntemi/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DenklemKöküBulma_DeltaYöntemi")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("DenklemKöküBulma_DeltaYöntemi")]
+[assembly: AssemblyCopyright("Copyright © HP 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("04bcbcb2-22b0-45f4-9728-d4050b0636f6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
