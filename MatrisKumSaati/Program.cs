5085c3b03cfc67a450efef1a723820b1f8148218
Rumeysa Başaran <126478668+rumeysabasaran@users.noreply.github.com>
2025-06-01 14:22:48 +0300

    Add files via upload

---
+++ MatrisKumSaati/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrisKumSaati
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("3'ten büyük bir sayı giriniz(Matris boyutu): ");
+            int n=int.Parse(Console.ReadLine());
+            int[,] dizi = new int[n, n];
+            Random rnd = new Random();
+            Console.WriteLine();
+            
+            Console.WriteLine("----------MATRİSİMİZ----------");
+            Console.WriteLine();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    dizi[i,j] = rnd.Next(1,10);
+                    Console.Write(dizi[i,j]+"  ");
+                }
+                Console.WriteLine();
+            }
+            int toplam = 0;
+            Console.WriteLine();
+            for (int i = 1; i <= (n - 1) / 2; i++)
+            {
+                for (int j = i;j<=(n-1-i);j++)
+                {
+                    toplam += dizi[i - 1, j] + dizi[n - i, j];
+                }
+            }
+            Console.WriteLine();
+            Console.Write("Kum saati(köşegenlerin üst ve altındaki alanların toplamı: " + toplam);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MatrisKumSaati/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MatrisKumSaati")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("MatrisKumSaati")]
+[assembly: AssemblyCopyright("Copyright © HP 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4ca96ca5-76c6-4de9-b810-afc71e4ad800")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
